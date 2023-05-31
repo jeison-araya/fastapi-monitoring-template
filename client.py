be09073301467ddf8fc44ba0160de899c62e1af0
Jeison Araya Mena <57369279+jeison-araya@users.noreply.github.com>
2023-05-30 21:58:24 -0600

    Client modified

--- client.py
+++ client.py
@@ -1,18 +1,9 @@
 import time 
 import requests
-
+import random
 url = "http://localhost:8000/"
 endpoint_1 = "delay/"
 endpoint_2 = "error/"
-
-while True:
-    try: 
-        time.sleep(1)
-        response = requests.get(url)
-        print(response.json())
-    except:
-        print("Error to connect with API")
-
 
 def call_endpoint_1():
     response = requests.get(url + endpoint_1)
@@ -22,3 +13,21 @@ def call_endpoint_1():
 def call_endpoint_2():
     response = requests.get(url + endpoint_2)
     print(response.json())
+
+
+while True:
+    try: 
+        time.sleep(2)
+        response = requests.get(url)
+        print(response.json())
+
+        for _ in range(random.randint(0, 3)):
+            call_endpoint_1()
+
+        time.sleep(3)
+
+        for _ in range(random.randint(0, 3)):
+            call_endpoint_2()
+    except:
+        print("Error to connect with API")
+
